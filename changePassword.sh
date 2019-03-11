2652fba4fce142a74841982dcdeb41e5517da4f1
Joseph Chenkie <josephchenjian@icloud.com>
2019-03-11 19:58:49 +0800

    Update changePassword.sh

--- changePassword.sh
+++ changePassword.sh
@@ -1,4 +1,5 @@
 sudo su -
 cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.org
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
 echo 'root:Chen123$' | chpasswd
